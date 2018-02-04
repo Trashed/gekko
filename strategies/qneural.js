738131ba1ae8fe71c4000278b97909954ea5d786
codetrasher <jkortelahti@gmail.com>
2018-02-04 23:12:49 +0200

    Added neural net layer definitions and options into q-trader. Refactored some training logic. Doesn't seem very promising.

--- strategies/qneural.js
+++ strategies/qneural.js
@@ -13,11 +13,6 @@ const deepqlearn = require('convnetjs/build/deepqlearn');
 
 var log = require('../core/log.js');
 
-// the below line is for calculating the last mean vs the now mean.
-var oldmean = 0
-getOption = function () {
-
-}
 
 
 // let's create our own method
@@ -29,6 +24,33 @@ var adxSettings = {
 };
 
 var rsiSettings = adxSettings;
+
+// Options for Q-learner
+var inputs = 4;
+var actions = 3;
+var temporalWindow = 4;
+var networkSize = inputs * temporalWindow + actions * temporalWindow + inputs; // 4 * 4 + 3 * 4 + 4 = 16 + 12 + 4 = 32
+// Network layer setup
+var layerDefs = [];
+layerDefs.push({type: 'input', out_sx: 1, out_sy: 1, out_depth: networkSize});
+layerDefs.push({type: 'fc', num_neurons: 20});
+layerDefs.push({type: 'svm', num_classes: actions});
+// Network trainer
+var trainerOpts = {learning_rate: 0.07, momentum: 0.0, batch_size: 32, l2_decay: 0.01};
+var opts = {};
+opts.temporal_window = temporalWindow;
+opts.experience_size = 3000;
+opts.start_learn_threshold = 5000;
+opts.gamma = 0.1;
+opts.learning_steps_total = 20000;
+opts.learning_steps_burnin = 3000;
+opts.epsilon_min = 0.05;
+opts.epsilon_test_time = 0.05;
+opts.layer_defs = layerDefs;
+opts.tdtrainer_options = trainerOpts;
+opts.threads = numCPUs;
+opts.learns = 2;
+
 
 
 // prepare everything our method needs
@@ -48,7 +70,7 @@ method.init = function() {
 
     // Create the 'brain' for Q-learning
     if (brain == undefined) {
-        brain = new deepqlearn.Brain(4, 3); // (4, 3)
+        brain = new deepqlearn.Brain(4, 3, opts); // (4, 3)
     }
 }
 
@@ -61,6 +83,8 @@ var count = 0;
 var candleCount = 0;
 var hasPredicted = false;
 var lastAction = 0; // Actions: 0 - hold, 1 - buy, 2 - sell
+var boughtPrice = 0;
+
 method.update = function(candle) {
 
     // console.log('\tcandle: ', candle);
@@ -92,8 +116,8 @@ method.update = function(candle) {
     // TODO: Handle Q-learning algo
     // Start training algorithm when there's more than one state array.
     log.debug('\t###### Training deepqlearn #######\n');
-    log.debug('\tadx: ', adx);
-    log.debug('\trsi: ', rsi);
+    // log.debug('\tadx: ', adx);
+    // log.debug('\trsi: ', rsi);
     log.debug('\tCount: ', count);
     if (States.length > 1) {
 
@@ -110,32 +134,31 @@ method.update = function(candle) {
         count++;
 
         var lastState = trainState;
+        var hasTrainBought = false;
         for (var i = 1; i < States.length - 1; i++) {
             trainState = States[i]
+            // console.log('trainState: ', trainState);
             action = brain.forward(trainState);
             // console.log('action: ', action);
 
+            reward = 0.0;
+
             // If Action is sell and current price is higher than last close price, reward is positive. Otherwise rewards is negative.
-            if (action == 2 ) {
-                if (lastAction == 2) {
-                    reward = -0.5;
-                } else if (trainState[0] > lastState[1]) {
-                    reward = 1.0;
-                } else {
-                    reward = -1.0;
+            if (action == 2 && hasTrainBought) {
+                hasTrainBought = false;
+                if (/*trainState[0] > (boughtPrice * 1.05) &&*/ trainState[2] > normalize(25, 0, 100) &&  trainState[3] > normalize(70, 0, 100)) {
+                    log.debug('\tcurr price: ', trainState[0], ' bought price: ', boughtPrice);
+                    reward = trainState[0] - boughtPrice;
+                    log.debug('\treward: ', reward);
                 }
-            } else if (action == 1) { // If action is buy and current price is lower than last price, reward is positive. Otherwise reward is negative.
-                if (lastAction == 1) {
-                    reward = -0.5;
-                } else if (trainState[0] < lastState[1]) {
-                    reward = 1.0;
-                } else {
-                    reward = -1.0;
+            } else if (action == 1 && !hasTrainBought) { // If action is buy and current price is lower than last price, reward is positive. Otherwise reward is negative.
+                hasTrainBought = true;
+                if (/*trainState[0] < lastState[1] &&*/ trainState[2] > normalize(25, 0, 100) &&  trainState[3] < normalize(30, 0, 100)) {
+                    boughtPrice = trainState[0];
+                    log.debug('\tboughtPrice: ', boughtPrice);
+                    // reward = 1.0;
                 }
-            } else {
-                reward = 0.0;
             }
-            // console.log('reward: ', reward);
             brain.backward([reward]);
 
             count++;
@@ -158,7 +181,7 @@ method.log = function() {
     // log.debug("prediction count:", predictioncount);
 }
 
-const MAX_PREDICTION_COUNT = 1000;
+const MAX_PREDICTION_COUNT = 5000;
 var hasbought = false;
 method.check = function() {
 
@@ -167,21 +190,22 @@ method.check = function() {
         var state = States[States.length - 1];
         var action = brain.forward(state);
 
-        log.debug('\t\tpredicting action: ', action);
+        log.debug('\tpredicting action: ', action);
 
-        if (action == 1) {
+        if (action == 1 && !hasbought) {
             log.debug('\tBUY ACTION TRIGGERED\n');
 
             hasbought = true;
             hasPredicted = false;
             this.advice('long');
-        } else if (action == 2) {
+        } else if (action == 2 && hasbought) {
             log.debug('\tSELL ACTION TRIGGERED\n')
 
             hasbought = false;
             hasPredicted = false;
             this.advice('short');
         } else {
+            log.debug('\tNO ACTION TRIGGERED\n')
             this.advice();
         }
 
